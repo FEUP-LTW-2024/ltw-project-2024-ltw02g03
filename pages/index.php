14321aa159e48f9253874c8eb7b59b283079380e
João Torres <rey.joao@hotmail.com>
2024-04-14 00:21:37 +0100

    Draw artist problem

--- pages/index.php
+++ pages/index.php
@@ -5,14 +5,14 @@
   $session = new Session();
 
   require_once(__DIR__ . '/../database/connection.db.php');
-  require_once(__DIR__ . '/../database/artist.class.php');
+  //require_once(__DIR__ . '/../database/artist.class.php');
 
   require_once(__DIR__ . '/../templates/common.tpl.php');
-  require_once(__DIR__ . '/../templates/artist.tpl.php');
+  //require_once(__DIR__ . '/../templates/artist.tpl.php');
 
   $db = getDatabaseConnection();
 
-  $artists = Artist::getArtists($db, 8);
+ // $artists = Artist::getArtists($db, 8);
 
   drawHeader($session);
   drawArtists($artists);
